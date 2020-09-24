cdfce9da0db0687cd9b531ad7992b51a2abf8208
AlexisBerrio <bealexis@hotmail.com>
2020-09-24 18:46:09 -0500

    Commented code and APK

--- app/src/main/java/com/alexisberrio/formulario/MainActivity.kt
+++ app/src/main/java/com/alexisberrio/formulario/MainActivity.kt
@@ -22,11 +22,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Put icon in the action bar
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.mipmap.ic_launcher)
 
+        // Set listener for the Date Picker
         val dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            DatePickerDialog.OnDateSetListener { _, year, month, day ->
                 c.set(Calendar.YEAR, year)
                 c.set(Calendar.MONTH, month)
                 c.set(Calendar.DAY_OF_MONTH, day)
@@ -34,9 +36,12 @@ class MainActivity : AppCompatActivity() {
                 val format = "MM/dd/yyyy"
                 val sdf = SimpleDateFormat(format, Locale.US)
                 fechaNacimiento = sdf.format(c.time).toString()
+
+                // Put date text on button
                 fecha_nacimiento_button.text = fechaNacimiento
             }
 
+        // Set listener for the button to call Date Picker
         fecha_nacimiento_button.setOnClickListener {
             DatePickerDialog(
                 this,
@@ -65,43 +70,56 @@ class MainActivity : AppCompatActivity() {
 
             val ciudadDeNacimiento = lugar_nacimieto_spinner.selectedItem
 
-            if (contrasena != repContrasena) {
-                contraseña_edit_text.error = getString(R.string.error_contrasena)
-            } else if (nombre.isEmpty()) nombre_edit_text.error = getString(R.string.campo_vacio)
-            else if (correo.isEmpty()) correo_edit_text.error = getString(R.string.campo_vacio)
-            else if (telefono.isEmpty()) telefono_edit_text.error = getString(R.string.campo_vacio)
-            else if (contrasena.isEmpty()) contraseña_edit_text.error =
-                getString(R.string.campo_vacio)
-            else if (repContrasena.isEmpty()) repcontraseña_edit_text.error =
-                getString(R.string.campo_vacio)
-            else if (ciudadDeNacimiento == "Seleccione una ciudad") {
-                Toast.makeText(
-                    applicationContext,
-                    getString(R.string.no_ciudad),
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else if (fechaNacimiento.isEmpty()) {
-                Toast.makeText(
-                    applicationContext,
-                    getString(R.string.no_fecha),
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                mostrar_textView.text = resources.getString(
-                    R.string.respuesta,
-                    nombre,
-                    correo,
-                    telefono,
-                    genero,
-                    pasatiempos,
-                    ciudadDeNacimiento,
-                    fechaNacimiento
-                )
-                Toast.makeText(
-                    applicationContext,
-                    getString(R.string.registro_guardado),
-                    Toast.LENGTH_SHORT
-                ).show()
+            // Detects empty edit texts and display the currently error
+            when {
+                contrasena != repContrasena -> {
+                    contraseña_edit_text.error = getString(R.string.error_contrasena)
+                }
+                nombre.isEmpty() -> nombre_edit_text.error = getString(R.string.campo_vacio)
+                correo.isEmpty() -> correo_edit_text.error = getString(R.string.campo_vacio)
+                telefono.isEmpty() -> telefono_edit_text.error = getString(R.string.campo_vacio)
+                contrasena.isEmpty() -> contraseña_edit_text.error =
+                    getString(R.string.campo_vacio)
+                repContrasena.isEmpty() -> repcontraseña_edit_text.error =
+                    getString(R.string.campo_vacio)
+
+                // Guarantee the choice of a city
+                ciudadDeNacimiento == "Seleccione una ciudad" -> {
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.no_ciudad),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                // Guarantee the choice of a date
+                fechaNacimiento.isEmpty() -> {
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.no_fecha),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                // If any error, display the new user information
+                else -> {
+                    mostrar_textView.text = resources.getString(
+                        R.string.respuesta,
+                        nombre,
+                        correo,
+                        telefono,
+                        genero,
+                        pasatiempos,
+                        ciudadDeNacimiento,
+                        fechaNacimiento
+                    )
+                    contraseña_edit_text.error = null
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.registro_guardado),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
 
